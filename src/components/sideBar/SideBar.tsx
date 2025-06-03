972ba6f8126bf1bf6a106f65c6f0a3530249faf6
qarnoldd <aqui0927@uni.sydney.edu.au>
2025-06-03 14:23:34 +1000

    icons updated to fix deploy error

--- src/components/sideBar/SideBar.tsx
+++ src/components/sideBar/SideBar.tsx
@@ -1,17 +1,21 @@
 import { useState } from "react";
 import "../../App.css";
 import { Link } from "react-router-dom";
+import { IconType } from "react-icons";
 import { FaHome } from "react-icons/fa";
 import { IoIosSettings } from "react-icons/io";
+
+const HomeIcon: IconType = FaHome;
+const SettingsIcon: IconType = IoIosSettings;
 
 function SideBar() {
   return (
     <div className="SideBar">
       <Link to="/">
-        <FaHome size={40} className="HomeIcon" />
+        <HomeIcon size={40} className="HomeIcon" />
       </Link>
       <Link to="/">
-        <IoIosSettings size={40} className="HomeIcon" />
+        <SettingsIcon size={40} className="HomeIcon" />
       </Link>
     </div>
   );
